f36d6af7c9ac7d9f11fbb69dd4c79a558a8e0518
Tao E. Li <t.e.li@outlook.com>
2025-09-30 09:55:32 -0400

    update lmp file

--- examples/run_meep_socketmolecule_lammps.sh
+++ examples/run_meep_socketmolecule_lammps.sh
@@ -5,4 +5,4 @@ python test_meep_socketmolecule_lammps.py &
 echo "After launching MEEP, sleeping 10 seconds to wait for the socket server to start..."
 sleep 10
 
-lmp < lammps_input/in.lmp
+lmp_mxl < lammps_input/in.lmp
