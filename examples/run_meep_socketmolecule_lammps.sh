31d6077c0a25c21d58f07fee443555c89566a5e3
Tao E. Li <t.e.li@outlook.com>
2025-10-20 15:57:14 -0400

    remove legacy examples

--- examples/run_meep_socketmolecule_lammps.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python meep_socketmolecule_lammps.py &
-
-echo "After launching MEEP, sleeping 10 seconds to wait for the socket server to start..."
-sleep 10
-
-lmp_mxl < lammps_input/in.lmp
--- examples/run_meep_socketmolecule_rtttdft.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python meep_socketmolecule_rttddft.py &
-
-echo "After launching MEEP, sleeping 10 seconds to wait for the socket server to start..."
-sleep 10
-
-mxl_driver.py --model rttddft --port 31880 --param "molecule_xyz=../tests/data/hcn.xyz, delta_kick_au=1e-3, checkpoint=false, restart=false" --verbose
