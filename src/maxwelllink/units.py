7a7f1cceebddb123c3e1372fbeb0c46b7a37744c
Tao E. Li <t.e.li@outlook.com>
2025-10-20 22:45:40 -0400

    cleanup units

--- src/maxwelllink/units.py
+++ src/maxwelllink/units.py
@@ -6,6 +6,6 @@ EV_TO_CM_INV = 8065.540106923572
 FS_INV_TO_EV = 4.135668  # 1fs-1 to 4.13 eV
 
 BOHR_PER_ANG = 1.889726124565062
-# E(a.u.) = 5.142206747e11 V/m; F(eV/Å) for q=1 is E(V/m) * 1e-10
-# So F(eV/Å) = q * E(a.u.) * 51.422067476  (≈ 5.1422e11 * 1e-10)
+# E(a.u.) = 5.142206747e11 V/m; F(eV/Angstrom) for q=1 is E(V/m) * 1e-10
+# So F(eV/Angstrom) = q * E(a.u.) * 51.422067476  (5.1422e11 * 1e-10)
 FORCE_PER_EFIELD_AU_EV_PER_ANG = 51.422067476
